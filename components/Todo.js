77a56002b272dbf093fbf061134b7f6ef7cbda3f
Marcos <marcos.d.perez@gmail.com>
2020-12-10 20:39:55 -0300

    Completar a nivel completo

--- components/Todo.js
+++ components/Todo.js
@@ -24,24 +24,28 @@ const styles = {
 export default function T({ text, id, completed }) {
   const dispatch = useDispatch();
   return (
-    <View style={{ ...styles.container, flexDirection: "row", marginTop: 8 }}>
-      <View style={{ flex: 2, paddingHorizontal: "8%" }}>
-        <TouchableHighlight
-          style={styles.item}
-          onPress={() => dispatch(toggleTodo(id))}
+    <View style={{ ...styles.container, marginTop: 8 }}>
+      <TouchableHighlight
+        style={styles.item}
+        onPress={() => dispatch(toggleTodo(id))}
+      >
+        <View
+          style={{ flex: 1, paddingHorizontal: "8%", flexDirection: "row" }}
         >
-          <Text style={styles.title}>
-            {id} - {text}
-          </Text>
-        </TouchableHighlight>
-      </View>
-      <View style={{ flex: 1, paddingHorizontal: "8%" }}>
-        {completed ? (
-          <Text style={styles.completed}>Completada</Text>
-        ) : (
-          <Text style={styles.incompleted}>Incompleta</Text>
-        )}
-      </View>
+          <View style={{ flex: 2 }}>
+            <Text style={styles.title}>
+              {id} - {text}
+            </Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            {completed ? (
+              <Text style={styles.completed}>Completada</Text>
+            ) : (
+              <Text style={styles.incompleted}>Incompleta</Text>
+            )}
+          </View>
+        </View>
+      </TouchableHighlight>
     </View>
   );
 }
